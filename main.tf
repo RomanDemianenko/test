f2d3fadbc1d987708e85dba74c1e4f14e71636e1
RomanDemianenko <romandemianenko192@gmail.com>
2023-12-22 14:24:20 +0000

    update

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "github_repository" {
-  source              = "github.com/RomanDemianenko/tf-github-repository"
-  github_owner        = var.GITHUB_OWNER
-  github_token        = var.GITHUB_TOKEN
-  repository_name     = var.FLUX_GITHUB_REPO
+  source                   = "github.com/RomanDemianenko/tf-github-repository"
+  github_owner             = var.GITHUB_OWNER
+  github_token             = var.GITHUB_TOKEN
+  repository_name          = var.FLUX_GITHUB_REPO
   public_key_openssh       = module.tls_private_key.public_key_openssh
   public_key_openssh_title = "flux-ssh-pub"
 }
@@ -23,6 +23,27 @@ module "flux_bootstrap" {
 }
 
 module "tls_private_key" {
-  source = "github.com/RomanDemianenko/tf-hashicorp-tls-keys"
-  algorithm   = "RSA"
+  source    = "github.com/RomanDemianenko/tf-hashicorp-tls-keys"
+  algorithm = "RSA"
+}
+
+module "gke-workload-identity" {
+  source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  use_existing_k8s_sa = true
+  name                = "kustomize-controller"
+  namespace           = "flux-system"
+  project_id          = var.GOOGLE_PROJECT
+  cluster_name        = "main"
+  location            = var.GOOGLE_REGION
+  annotate_k8s_sa     = true
+  roles               = ["roles/cloudkms.cryptoKeyEncrypterDecrypter"]
+}
+
+module "kms" {
+  source          = "github.com/RomanDemianenko/terraform-google-kms"
+  project_id      = var.GOOGLE_PROJECT
+  keyring         = "sops-flux"
+  location        = "global"
+  keys            = ["sops-key-flux"]
+  prevent_destroy = false
 }
